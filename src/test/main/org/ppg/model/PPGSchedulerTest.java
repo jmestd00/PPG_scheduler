1f74e9e24ab346451684e9f28ed722efec9e5075
jmestd <jmestd00@estudiantes.unileon.es>
2024-10-30 15:37:25 +0100

    Añado los tests

---
+++ src/test/main/org/ppg/model/PPGSchedulerTest.java
@@ -0,0 +1,26 @@
+package org.ppg.model;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertTrue;
+
+public class PPGSchedulerTest {
+
+    // TODO Hay que implementar los test
+
+    @Before
+    public void setUp() {
+
+    }
+
+    @Test /* (expected = PPGSchedulerException.class) */
+    public void testTemplate() /* throws PPGSchedulerException */ {
+        /*
+         * assertTrue();
+         * assertFalse();
+         * assertEquals();
+         * ...
+         */
+    }
+}
