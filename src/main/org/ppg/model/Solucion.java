0f517a9bf82bcbd0a54b3630a2cf39bf3ac5aa25
rsevis00 <rsevis00@estudiantes.unileon.es>
2024-11-13 14:57:50 +0100

    Clase Solucion para algoritmo

---
+++ src/main/org/ppg/model/Solucion.java
@@ -0,0 +1,23 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class Solucion {
+    private ArrayList<Lote> lotesSolucion;
+
+
+    public Solucion(ArrayList<Lote> lotes) {
+        this.lotesSolucion = new ArrayList<>();
+        for (Lote lote : lotes) {
+            this.lotesSolucion.add(lote.clone()); // Copia profunda
+        }
+    }
+
+    public ArrayList getLotes() {
+        return this.lotesSolucion;
+    }
+
+    @Override
+    public String toString() {
+        return "Solucion{lotes=" + lotesSolucion + "}";
+    }
+}
