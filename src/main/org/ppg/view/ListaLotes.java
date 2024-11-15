c1b0e869dfe17d6c2cb7d6a2766edba9dfd71c69
DFernJ <dfernj02@estudiantes.unileon.es>
2024-11-15 12:08:14 +0100

    Añadidos pngs para añadir iconos a la aplicación, la interfaz de editar lote junto con su css

--- src/main/org/ppg/view/ListaLotes.java
+++ src/main/org/ppg/view/ListaLotes.java
@@ -1,6 +1,10 @@
 package org.ppg.view;
 
+import de.jensd.fx.glyphs.fontawesome.FontAwesomeIcon;
+import de.jensd.fx.glyphs.fontawesome.FontAwesomeIconView;
 import javafx.beans.property.StringProperty;
+import javafx.scene.image.Image;
+import javafx.scene.shape.SVGPath;
 import org.ppg.model.*;
 
 import javafx.application.Application;
@@ -20,56 +24,61 @@ import javafx.util.Callback;
 
 import java.util.Objects;
 
+
+
 public class ListaLotes extends Application {
+    Image logoPPG = new Image(String.valueOf(getClass().getResource("/images/PPG_Logo512_512.png")));
     @FXML
     private AnchorPane rootPane;
-    
+
     @FXML
     private Label titleLabel;
-    
+
     @FXML
     private Button addButton;
-    
+
     @FXML
     private TableView<Lote> tableView;
-    
+
     @FXML
     private TableColumn<Lote, String> pClassCol;
-    
+
     @FXML
     private TableColumn<Lote, String> plantCol;
-    
+
     @FXML
     private TableColumn<Lote, String> itemCol;
-    
+
     @FXML
     private TableColumn<Lote, String> iDateCol;
-    
+
     @FXML
     private TableColumn<Lote, String> nDateCol;
-    
+
     @FXML
     private TableColumn<Lote, String> nLoteCol;
-    
+
     @FXML
     private TableColumn<Lote, String> statusCol;
-    
+
     @FXML
-    private TableColumn<Lote, String> quantityCol;
-    
+    private  TableColumn<Lote, String> quantityCol;
+
     @FXML
-    private TableColumn<Button, Void> editCol; // Columna para botones de edición
-    
+    private TableColumn<Lote, Void> editCol; // Columna para botones de edición
+
     @FXML
     private Pagination pagination;
-    
+
     @FXML
     private Stage stageActual;
-    
+
     private final ObservableList<Lote> loteData = FXCollections.observableArrayList();
-    
+
     private final int ROWS_PER_PAGE = 11; // Número de elementos por página
-    
+
+
+
     public void initialize() {
         setupData();
         // Configuración de las columnas
@@ -82,24 +91,35 @@ public class ListaLotes extends Application {
         nDateCol.setCellValueFactory(data -> data.getValue().getProperties()[6]);
         statusCol.setCellValueFactory(data -> data.getValue().getProperties()[7]);
         addTooltipToCells(nLoteCol, 8);
-        
         // Configuración del botón en buttonNameColumn
         editCol.setCellFactory(param -> new TableCell<>() {
-            private final Button btn = new Button("EDITAR");
+            private final Button btn = new Button("");
+
             {
                 // Define la acción del botón
                 btn.setOnAction(event -> {
-                
+                    Lote selectedLote = getTableView().getItems().get(getIndex());
+                    abrirEditarLote(selectedLote);
                 });
-                btn.setStyle("-fx-background-color: #ffffff; -fx-background-radius: 0; -fx-alignment: " + "center; -fx-max-width: 130px; -fx-pref-height: 61px; -fx-padding: 0;" + "-fx-border-color: transparent; ");
-                
-                btn.setOnMouseEntered(event -> btn.setStyle("-fx-background-color: #FFD700; -fx-background-radius: 0; -fx-alignment: " + "center; -fx-max-width: 130px; -fx-pref-height: 61px; -fx-padding: 0;" + "-fx-border-color: transparent; -fx-transition: all 0.3s ease;"));
-                
-                btn.setOnMouseExited(event -> btn.setStyle("-fx-background-color: #ffffff; -fx-background-radius: 0; -fx-alignment: " + "center; -fx-max-width: 130px; -fx-pref-height: 61px; -fx-padding: 0;" + "-fx-border-color: transparent; "));
+                btn.setStyle("/*-fx-background-color: #ffffff;*/ -fx-background-radius: 0; -fx-alignment: " +
+                        "center; -fx-max-width: 130px; -fx-pref-height: 61px; -fx-padding: 0;" +
+                        "    -fx-background-image: url('/images/Engranaje128_128.png');" +
+                        "    -fx-background-size: 60px 60px;" +
+                        " -fx-background-color: transparent;" +
+                        " -fx-background-position: center");
+
+/*
+                btn.setOnMouseEntered(event -> btn.setStyle("-fx-background-color: #FFD700; -fx-background-radius: 0; -fx-alignment: " +
+                        "center; -fx-max-width: 130px; -fx-pref-height: 61px; -fx-padding: 0;" +
+                        "-fx-border-color: transparent; -fx-transition: all 0.3s ease;"));
+
+                btn.setOnMouseExited(event -> btn.setStyle("-fx-background-color: #ffffff; -fx-background-radius: 0; -fx-alignment: " +
+                        "center; -fx-max-width: 130px; -fx-pref-height: 61px; -fx-padding: 0;" +
+                        "-fx-border-color: transparent; "));*/
                 btn.setPrefHeight(Double.MAX_VALUE);
                 btn.setPrefHeight(Double.MAX_VALUE);
             }
-            
+
             protected void updateItem(Void item, boolean empty) {
                 super.updateItem(item, empty);
                 if (empty) {
@@ -111,51 +131,61 @@ public class ListaLotes extends Application {
                 }
             }
         });
-        
+
         // Configuración de la paginación
         int totalPage = (int) (Math.ceil(loteData.size() * 1.0 / ROWS_PER_PAGE));
         pagination.setPageCount(totalPage);
         pagination.setCurrentPageIndex(0);
         changeTableView(0, ROWS_PER_PAGE);
-        pagination.currentPageIndexProperty().addListener((observable, oldValue, newValue) -> changeTableView(newValue.intValue(), ROWS_PER_PAGE));
+        pagination.currentPageIndexProperty().addListener(
+                (observable, oldValue, newValue) -> changeTableView(newValue.intValue(), ROWS_PER_PAGE));
     }
-    
+
+
     public void setupData() {
-        loteData.add(new Lote(20000000, "A", "A", "A", 5, new Date(1, 2, 2000), new Date(1, 2, 2000), Estados.EN_ESPERA, "A", Types.PIMM));
-        loteData.add(new Lote(2, "VD-WBBC", "VDW", "A-RXX3359-DD", 10, new Date(15, 2, 2023), new Date(15, 2, 2023), Estados.FINALIZADO, "a", Types.PIMM));
+        loteData.add(new Lote(20000000, "A", "A", "A", 5, new Date(1,2,2000), new Date(1,2,2000), Estados.EN_ESPERA, "A", "A"));
+        loteData.add(new Lote(2, "VD-WBBC","VDW" ,"A-RXX3359-DD", 10, new Date(15,2,2023), new Date(15,2,2023), Estados.FINALIZADO, "a" ,"a"));
         for (int i = 3; i < 500; i++) {
-            loteData.add(new Lote(i + 27000000, "VD-WBBC" + i, "VDW", "A-RXX3359-DD", 10, new Date(15, 2, 2023), new Date(15, 2, 2023), Estados.FINALIZADO, "Esto es la descripción. ", Types.PISC));
+            loteData.add(new Lote(i + 27000000, "VD-WBBC" + i,"VDW" ,"A-RXX3359-DD", 10,new Date(15,2,2023), new Date(15,2,2023), Estados.EN_ESPERA, "Esto es la descripción. " , "A"));
         }
     }
-    
+
+
     private void changeTableView(int index, int limit) {
+
+
         int fromIndex = index * limit;
         int toIndex = Math.min(fromIndex + limit, loteData.size());
-        
+
         int minIndex = Math.min(toIndex, loteData.size());
-        SortedList<Lote> sortedData = new SortedList<>(FXCollections.observableArrayList(loteData.subList(Math.min(fromIndex, minIndex), minIndex)));
+        SortedList<Lote> sortedData = new SortedList<>(
+                FXCollections.observableArrayList(loteData.subList(Math.min(fromIndex, minIndex), minIndex)));
         sortedData.comparatorProperty().bind(tableView.comparatorProperty());
+
+
         tableView.setItems(sortedData);
+
     }
-    
+
     @FXML
     private void abrirNuevoLote() {
         try {
             // Cargar el archivo FXML del popup
             FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("/fxml/nuevoLote.fxml"));
             Parent popupRoot = fxmlLoader.load();
-            
             // Crear una nueva ventana para el popup
             Stage popupStage = new Stage();
             popupStage.resizableProperty().setValue(Boolean.FALSE);
             popupStage.setTitle("Insertar nuevo lote");
+            popupStage.getIcons().add(logoPPG);
+
             popupStage.initModality(Modality.APPLICATION_MODAL); // Bloquear la ventana principal
             popupStage.setScene(new Scene(popupRoot));
             popupStage.setOnShown(event -> {
                 // Obtener dimensiones de la ventana principal o pantalla
                 double centerX = addButton.getScene().getWindow().getX() + addButton.getScene().getWindow().getWidth() / 2;
                 double centerY = addButton.getScene().getWindow().getY() + addButton.getScene().getWindow().getHeight() / 2;
-                
+
                 // Calcular posición para centrar el popup
                 popupStage.setX(centerX - popupStage.getWidth() / 2);
                 popupStage.setY(centerY - popupStage.getHeight() / 2);
@@ -166,7 +196,39 @@ public class ListaLotes extends Application {
             e.printStackTrace();
         }
     }
-    
+
+    @FXML
+    private void abrirEditarLote(Lote sampleLote) {
+        try {
+            // Cargar el archivo FXML del popup
+            FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("/fxml/editarLote.fxml"));
+            Parent popupRoot = fxmlLoader.load();
+            EditarLote editarLote = fxmlLoader.getController();
+            // Pasar el objeto Lote al controlador de la ventana de edición
+            editarLote.setLote(sampleLote);
+            // Crear una nueva ventana para el popup
+            Stage popupStage = new Stage();
+            popupStage.resizableProperty().setValue(Boolean.FALSE);
+            popupStage.setTitle("Editar lote");
+            popupStage.getIcons().add(logoPPG);
+
+            popupStage.initModality(Modality.APPLICATION_MODAL); // Bloquear la ventana principal
+            popupStage.setScene(new Scene(popupRoot));
+            popupStage.setOnShown(event -> {
+                // Obtener dimensiones de la ventana principal o pantalla
+                double centerX = addButton.getScene().getWindow().getX() + addButton.getScene().getWindow().getWidth() / 2;
+                double centerY = addButton.getScene().getWindow().getY() + addButton.getScene().getWindow().getHeight() / 2;
+
+                // Calcular posición para centrar el popup
+                popupStage.setX(centerX - popupStage.getWidth() / 2);
+                popupStage.setY(centerY - popupStage.getHeight() / 2);
+            });
+            // Mostrar el popup
+            popupStage.showAndWait();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
     private void addTooltipToCells(TableColumn<Lote, String> column, int propertyIndex) {
         column.setCellFactory(new Callback<>() {
             @Override
@@ -174,36 +236,38 @@ public class ListaLotes extends Application {
                 return new TableCell<>() {
                     @Override
                     protected void updateItem(String item, boolean empty) {
-                    super.updateItem(item, empty);
-                    if (empty || item == null) {
-                        setText(null);
-                        setTooltip(null);
-                    } else {
-                        setText(item);
-                        
-                        // Crear y configurar el Tooltip con el valor de la celda
-                        Lote lote = getTableView().getItems().get(getIndex());
-                        StringProperty tooltipText = lote.getProperties()[propertyIndex];
-                        Tooltip tooltip = new Tooltip(tooltipText.getValue());
-                        setTooltip(tooltip);
-                    }
+                        super.updateItem(item, empty);
+
+                        if (empty || item == null) {
+                            setText(null);
+                            setTooltip(null);
+                        } else {
+                            setText(item);
+
+                            // Crear y configurar el Tooltip con el valor de la celda
+                            Lote lote = getTableView().getItems().get(getIndex());
+                            StringProperty tooltipText = lote.getProperties()[propertyIndex];
+                            Tooltip tooltip = new Tooltip(tooltipText.getValue());
+                            setTooltip(tooltip);
+                        }
                     }
                 };
             }
         });
     }
-    
+
     @Override
     public void start(Stage primaryStage) throws Exception {
         Parent root = FXMLLoader.load(Objects.requireNonNull(getClass().getResource("/fxml/listaLotes.fxml")));
         Scene scene = new Scene(root);
-        
+
         primaryStage.setTitle("PPG - Lista de Lotes");
         primaryStage.setScene(scene);
+        primaryStage.getIcons().add(logoPPG);
         primaryStage.resizableProperty().setValue(Boolean.FALSE);
         primaryStage.show();
     }
-    
+
     public static void main(String[] args) {
         launch(args);
     }
