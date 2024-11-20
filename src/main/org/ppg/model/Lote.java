0bff7a9a2a6f6f48f3a05c5542a78736487b3334
jngomm00 <jngomm00@estudiantes.unileon.es>
2024-11-20 13:53:25 +0100

    He adaptado la clase Lote a los requisitos de la vista

--- src/main/org/ppg/model/Lote.java
+++ src/main/org/ppg/model/Lote.java
@@ -1,94 +1,29 @@
 package org.ppg.model;
 
-public class Lote {
-    private Estados estado;
-    private Date fechaNecesidad;
-    private Date fechaInicio;
-    private Date fechaFinal;
-    private String tipo, planta, planningClass, item;
-    private int cantidad;
-    private final int id;
-    private int idDiluidor;
-    private int stock;
+import javafx.beans.property.SimpleStringProperty;
+import javafx.beans.property.StringProperty;
 
-    public Lote(int id, String planningClass, String planta, String item, int cantidad, String tipo,
-            Date fechaInicio, Date fechaFinal, Date fechaNecesidad, Estados estado,  int idDiluidor, int stock) {
-        this.id = id;
-        this.planningClass = planningClass;
-        this.planta = planta;
-        this.item = item;
-        this.fechaInicio = fechaInicio;
-        this.fechaFinal = fechaFinal;
-        this.fechaNecesidad = fechaNecesidad;
-        this.estado = estado;
-        this.tipo = tipo;
-        this.cantidad = cantidad;
-        this.idDiluidor = idDiluidor;
-        this.stock = stock;
+import java.time.LocalDate;
+
+public record Lote(int nLote, String planningClass, String plant, String item, int quantity, LocalDate fStart, LocalDate fNeed, Estados estado, String descriptions, String type) {
+
+    public StringProperty[] getProperties(){
+        StringProperty[] properties = new StringProperty[8];
+        properties[0] = new SimpleStringProperty(Integer.toString(nLote));
+        properties[1] = new SimpleStringProperty(planningClass);
+        properties[2] = new SimpleStringProperty(plant);
+        properties[3] = new SimpleStringProperty(item);
+        properties[4] = new SimpleStringProperty(Integer.toString(quantity));
+        properties[5] = new SimpleStringProperty(fStart.toString());
+        properties[6] = new SimpleStringProperty(fNeed.toString());
+        properties[7] = new SimpleStringProperty(descriptions);
+        return properties;
     }
-
-    public int getCantidad() {
-        return this.cantidad;
-    }
-
 
     @Override
-    public String toString() {
-        return "Lote{" +
-                "id=" + id +
-                ", estados=" + estado +
-                ", fechaNecesidad=" + fechaNecesidad +
-                ", fechaInicio=" + fechaInicio +
-                ", fechaFinal=" + fechaFinal +
-                ", tipo='" + tipo + '\'' +
-                ", planta='" + planta + '\'' +
-                ", planningClass='" + planningClass + '\'' +
-                ", item='" + item + '\'' +
-                ", cantidad=" + cantidad +
-                '}';
+    public Lote clone(){
+        Lote clone = new Lote(nLote,planningClass, plant, item,quantity, fStart, fNeed, estado, descriptions, type);
+        return clone;
     }
 
-    public String getTipo() {
-        return this.tipo;
-    }
-
-    public String getPlant() {
-        return this.planta;
-    }
-
-    public int getIdDiluidor() {
-        return this.idDiluidor;
-    }
-
-    public String getPlannigClass() {
-        return this.planningClass;
-    }
-
-    public String getEstado() {
-        return this.estado.getValue();
-    }
-
-    public String getItem() {
-        return this.item;
-    }
-
-    public int getId() {
-        return this.id;
-    }
-
-    public Date getFechaInicio() {
-        return this.fechaInicio;
-    }
-
-    public Date getFechaFinal() {
-        return this.fechaFinal;
-    }
-
-    public Date getFechaNecesidad() {
-        return this.fechaNecesidad;
-    }
-
-    public int getStock() {
-        return this.stock;
-    }
 }
