db2508df520267df6d0c25cafc033cea15e61752
rsevis00 <rsevis00@estudiantes.unileon.es>
2024-11-20 13:38:14 +0100

    Cambios clase solucion

--- src/main/org/ppg/model/Solucion.java
+++ src/main/org/ppg/model/Solucion.java
@@ -5,6 +5,8 @@ import java.util.List;
 
 public class Solucion {
     private ArrayList<Lote> lotesSolucion;
+    public static int idSolucionnext;
+    private int idSolucion;
 
 
     public Solucion(ArrayList<Lote> lotes) {
@@ -14,8 +16,43 @@ public class Solucion {
         }
     }
 
-    public ArrayList getLotes() {
+    public ArrayList<Lote> getLotes() {
         return this.lotesSolucion;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) return true; // Mismo objeto
+        if (obj == null || getClass() != obj.getClass()) return false; // Tipos diferentes
+        Solucion otra = (Solucion) obj;
+    
+        // Comparar tamaños de las listas
+        if (this.lotesSolucion.size() != otra.lotesSolucion.size()) {
+            return false;
+        }
+    
+        // Comparar lotes uno por uno (orden importa)
+        for (int i = 0; i < this.lotesSolucion.size(); i++) {
+            Lote lote1 = this.lotesSolucion.get(i);
+            Lote lote2 = otra.lotesSolucion.get(i);
+            if (!lote1.equals(lote2)) { // Asegúrate de que Lote también sobrescriba equals correctamente
+                return false;
+            }
+        }
+    
+        return true; // Todas las comparaciones pasaron
+    }
+    /**
+     * @Override
+    public boolean equals(Object obj) {
+        return this.idSolucion==((Solucion)obj).idSolucion;
+    }
+     */
+    
+
+    @Override
+    public int hashCode() {
+        return lotesSolucion.hashCode(); // Usa el hashCode de la lista de lotes
     }
 
     @Override
